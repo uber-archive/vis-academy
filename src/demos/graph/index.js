c5619f62c4c7ed549f0c4ac1c1a3a2e608804486
Javid Hsueh <javidhsueh@gmail.com>
2017-09-25 00:40:17 -0700

    add index.js

---
+++ src/demos/graph/index.js
@@ -0,0 +1,11 @@
+import StartingCode from './1-starting-code/app';
+import BasicGraph from './2-basic-graph/app';
+import GraphLayout from './3-graph-layout/app';
+import InteractiveGraph from './4-interactive-graph/app';
+
+export default {
+  StartingCode,
+  BasicGraph,
+  GraphLayout,
+  InteractiveGraph
+}
