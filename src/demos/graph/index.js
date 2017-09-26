4935e4f58b00c0afd275d7a0d0cea97e66443868
Javid Hsueh <javidhsueh@gmail.com>
2017-09-26 15:58:58 -0700

    Update index.js

--- src/demos/graph/index.js
+++ src/demos/graph/index.js
@@ -1,11 +1,15 @@
-import StartingCode from './1-starting-code/app';
-import BasicGraph from './2-basic-graph/app';
-import GraphLayout from './3-graph-layout/app';
-import InteractiveGraph from './4-interactive-graph/app';
+import StartingCode from './0-starting-code/src/app';
+import BasicGraph from './1-basic-graph/src/app';
+import GraphLayout from './2-graph-layout/src/app';
+import InteractiveGraph from './3-interactive-graph/src/app';
+import FinalGraph from './4-final-version/src/app';
 
 export default {
+
   StartingCode,
   BasicGraph,
   GraphLayout,
-  InteractiveGraph
+  InteractiveGraph,
+  FinalGraph
+
 }
