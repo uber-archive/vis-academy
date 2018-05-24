5a7e0514175ece03e944e0b5a825261adbdf5b7a
Giuseppe Macri <gmacri@uber.com>
2018-05-24 08:38:25 -0700

    Added kepler.gl demo

---
+++ src/demos/kepler.gl/src/app.js
@@ -0,0 +1,157 @@
+// Copyright (c) 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import React, {Component} from 'react';
+import window from 'global/window';
+import {connect} from 'react-redux';
+import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
+
+import {loadSampleConfigurations} from './actions';
+import {replaceLoadDataModal} from './factories/load-data-modal';
+
+const KeplerGl = require('kepler.gl/components').injectComponents([
+  replaceLoadDataModal()
+]);
+
+const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+
+// Sample data
+/* eslint-disable no-unused-vars */
+import sampleTripData from './data/sample-trip-data';
+import sampleGeojson from './data/sample-geojson.json';
+import sampleIconCsv, {config as savedMapConfig} from './data/sample-icon-csv';
+import {updateVisData, addDataToMap} from 'kepler.gl/actions';
+import Processors from 'kepler.gl/processors';
+/* eslint-enable no-unused-vars */
+
+class App extends Component {
+  componentWillMount() {
+    // if we pass an id as part f the url
+    // we ry to fetch along map configurations
+    const {params: {id: sampleMapId} = {}} = this.props;
+    this.props.dispatch(loadSampleConfigurations(sampleMapId));
+
+    if (window.gtag) {
+      const {token} = this.props.location.query;
+      window.gtag('set', {token});
+
+      window.gtag('config', 'UA-64694404-19', {
+        user_id: token,
+        custom_map: {dimension1: 'token'}
+      });
+
+      window.gtag('send', 'pageview', {token});
+    }
+  }
+
+  componentDidMount() {
+    /**
+     * ENABLE THE FOLLOWING CODE TO PRE-POLUATE KEPLER.GL INSTANCE
+     */
+
+    // load trip based data with config
+    // this._loadSampleData();
+  }
+
+  _loadSampleData() {
+    this.props.dispatch(
+      updateVisData(
+        // datasets
+        {
+          info: {
+            label: 'Sample Taxi Trips in New York City',
+            id: 'test_trip_data'
+          },
+          data: sampleTripData
+        },
+        // option
+        {
+          centerMap: true,
+          readOnly: false
+        },
+        // config
+        {
+          filters: [
+            {
+              id: 'me',
+              dataId: 'test_trip_data',
+              name: 'tpep_pickup_datetime',
+              type: 'timeRange',
+              enlarged: true
+            }
+          ]
+        }
+      )
+    );
+
+    // load icon data and config and process csv file
+    this.props.dispatch(
+      addDataToMap({
+        datasets: [
+          {
+            info: {
+              label: 'Icon Data',
+              id: 'test_icon_data'
+            },
+            data: Processors.processCsvData(sampleIconCsv)
+          }
+        ],
+        options: {
+          centerMap: false
+        },
+        config: savedMapConfig
+      })
+    );
+
+    // load geojson
+    this.props.dispatch(
+      updateVisData({
+        info: {label: 'SF Zip Geo'},
+        data: Processors.processGeojson(sampleGeojson)
+      })
+    );
+  }
+
+  render() {
+    return (
+      <div style={{position: 'absolute', width: '100%', height: '100%'}}>
+        <AutoSizer>
+          {({height, width}) => (
+            <KeplerGl
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+              id="map"
+              /*
+               * Specify path to keplerGl state, because it is not mount at the root
+               */
+              getState={state => state.demo.keplerGl}
+              width={width}
+              height={height}
+            />
+          )}
+        </AutoSizer>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => state;
+const dispatchToProps = dispatch => ({dispatch});
+
+export default connect(mapStateToProps, dispatchToProps)(App);
