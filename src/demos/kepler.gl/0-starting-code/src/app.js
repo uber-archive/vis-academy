cf9ee2763ea47bd1e97107230f711209e1ec3b88
Shan He <shan@uber.com>
2018-05-29 12:52:19 -0700

    fix React import

--- src/demos/kepler.gl/0-starting-code/src/app.js
+++ src/demos/kepler.gl/0-starting-code/src/app.js
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
