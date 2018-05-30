4f9a368ea5f05946afeec6aa6f3dce39be26d492
Chris Chua <chua@uber.com>
2018-05-29 20:16:47 -0700

    fix the minimum height of the kepler demos

--- src/demos/kepler.gl/3-load-config/src/app.js
+++ src/demos/kepler.gl/3-load-config/src/app.js
@@ -104,7 +104,7 @@ class App extends Component {
 
   render() {
     return (
-      <div style={{position: 'absolute', width: '100%', height: '100%'}}>
+      <div style={{position: 'absolute', width: '100%', height: '100%', minHeight: '70vh'}}>
         <Button onClick={this.replaceData}>Replace Data</Button>
         <AutoSizer>
           {({height, width}) => (
