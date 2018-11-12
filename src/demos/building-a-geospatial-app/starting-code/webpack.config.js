e62bd0ebbdeaa4bd00c8c7c22015bdc1bf5fb6eb
Jerome Cukier <jerome@uber.com>
2018-11-12 09:50:15 -0800

    upgraded scaffold from buble to babel 7, react to react 16, all vis libraries to latest version

--- src/demos/building-a-geospatial-app/starting-code/webpack.config.js
+++ src/demos/building-a-geospatial-app/starting-code/webpack.config.js
@@ -2,39 +2,39 @@ const resolve = require('path').resolve;
 const webpack = require('webpack');
 
 module.exports = {
-  entry: resolve('./root.js'),
-
-  devtool: 'source-map',
-
-  module: {
-    rules: [{
-      // Compile ES2015 using buble
-      test: /\.js$/,
-      loader: 'buble-loader',
-      include: [resolve('.')],
-      exclude: [/node_modules/],
-      options: {
-        objectAssign: 'Object.assign'
-      }
-    }]
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
   resolve: {
+    extensions: ['*', '.js', '.jsx'],
     alias: {
       'mapbox-gl$': resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
     }
   },
-
   plugins: [
-
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
         MapboxAccessToken: JSON.stringify(
           process.env.MapboxAccessToken || process.env.MAPBOX_TOKEN
-        ),
-      },
-    }),
-
-  ],
+        )
+      }
+    })],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
 };
